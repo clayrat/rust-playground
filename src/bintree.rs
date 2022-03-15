ecd9bbe53b3f1d63d4766900a141c76ebe99190b
Alex Gryzlov <aliaksandr.hryzlou@imdea.org>
2022-03-15 14:40:00 +0100

    initial commit

---
+++ src/bintree.rs
@@ -0,0 +1,164 @@
+#[derive(Clone, Debug)]
+pub enum BinaryTree<T> {
+    Empty,
+    NonEmpty(Box<TreeNode<T>>),
+}
+
+#[derive(Clone, Debug)]
+pub struct TreeNode<T> {
+    pub left: BinaryTree<T>,
+    pub element: T,
+    pub right: BinaryTree<T>,
+}
+
+use std::cmp::Ordering::*;
+use BinaryTree::*;
+
+impl<T: Ord> BinaryTree<T> {
+    pub fn create(left: Self, value: T, right: Self) -> Self {
+        NonEmpty(Box::new(TreeNode {
+            left: left,
+            element: value,
+            right: right,
+        }))
+    }
+
+    pub fn add(&mut self, value: T) {
+        match *self {
+            Empty => *self = Self::create(Empty, value, Empty),
+            NonEmpty(ref mut node) => match value.cmp(&node.element) {
+                Equal => (),
+                Less => node.left.add(value),
+                Greater => node.right.add(value),
+            },
+        }
+    }
+
+    pub fn contains(&self, value: T) -> bool {
+        match self {
+            Empty => false,
+            NonEmpty(node) => match value.cmp(&node.element) {
+                Equal => true,
+                Less => node.left.contains(value),
+                Greater => node.right.contains(value),
+            },
+        }
+    }
+
+    fn inorder_sub(self, ret: &mut Vec<T>) {
+        match self {
+            Empty => (),
+            NonEmpty(node) => {
+                node.left.inorder_sub(ret);
+                ret.push(node.element);
+                node.right.inorder_sub(ret);
+            }
+        }
+    }
+    pub fn inorder(self) -> Vec<T> {
+        let mut ret = Vec::new();
+        self.inorder_sub(&mut ret);
+        ret
+    }
+
+    fn split_min(self) -> (T, BinaryTree<T>) {
+        match self {
+            Empty => panic!(),
+            NonEmpty(mut node) => {
+                if let Empty = node.left {
+                    (node.element, node.right)
+                } else {
+                    let (x, l1) = node.left.split_min();
+                    node.left = l1;
+                    (x, NonEmpty(node))
+                }
+            }
+        }
+    }
+
+    pub fn delete(self, value: T) -> (Self, bool) {
+        match self {
+            Empty => (self, false),
+            NonEmpty(node) => {
+                let TreeNode {
+                    left,
+                    element,
+                    right,
+                } = *node;
+                match value.cmp(&element) {
+                    Equal => {
+                        if let Empty = right {
+                            (left, true)
+                        } else {
+                            let (x, r1) = right.split_min();
+                            (Self::create(left, x, r1), true)
+                        }
+                    }
+                    Less => {
+                        let (t, b) = left.delete(value);
+                        (Self::create(t, element, right), b)
+                    }
+                    Greater => {
+                        let (t, b) = right.delete(value);
+                        (Self::create(left, element, t), b)
+                    }
+                }
+            }
+        }
+    }
+
+    fn join(t1: Self, t2: Self) -> Self {
+        match t1 {
+            Empty => t2,
+            NonEmpty(node1) => match t2 {
+                Empty => NonEmpty(node1),
+                NonEmpty(node2) => {
+                    let TreeNode {
+                        left: l1,
+                        element: a,
+                        right: r1,
+                    } = *node1;
+                    let TreeNode {
+                        left: l2,
+                        element: b,
+                        right: r2,
+                    } = *node2;
+                    if let NonEmpty(node3) = Self::join(r1, l2) {
+                        Self::create(
+                            Self::create(l1, a, node3.left),
+                            node3.element,
+                            Self::create(node3.right, b, r2),
+                        )
+                    } else {
+                        Self::create(l1, a, (Self::create(Empty, b, r2)))
+                    }
+                }
+            },
+        }
+    }
+
+    pub fn delete2(self, value: T) -> (Self, bool) {
+        match self {
+            Empty => (self, false),
+            NonEmpty(node) => {
+                let TreeNode {
+                    left,
+                    element,
+                    right,
+                } = *node;
+                match value.cmp(&element) {
+                    Equal => (Self::join(left, right), true),
+                    Less => {
+                        let (t, b) = left.delete(value);
+                        (Self::create(t, element, right), b)
+                    }
+                    Greater => {
+                        let (t, b) = right.delete(value);
+                        (Self::create(left, element, t), b)
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+pub mod bintree;
+use bintree::BinaryTree;
+use bintree::BinaryTree::*;
+use bintree::TreeNode;
+
+mod twothree;
+
+fn main() {
+    let jupiter_tree = NonEmpty(Box::new(TreeNode {
+        element: "Jupiter",
+        left: Empty,
+        right: Empty,
+    }));
+
+    let mut tree = BinaryTree::Empty;
+    tree.add("Venus");
+    tree.add("Jupiter");
+    tree.add("Mercury");
+    tree.add("Mars");
+    tree.add("Earth");
+    tree.add("Saturn");
+    tree.add("Neptune");
+    tree.add("Uranus");
+
+    println!("{:?}", tree.clone().inorder());
+
+    let (tree,b) = tree.delete("Neptune");
+    println!("{:?} : {}", tree.clone().inorder(), b);
+
+    let (tree,b) = tree.delete("Eris");
+    println!("{:?} : {}", tree.clone().inorder(), b);
+
+    println!("{}", tree.contains("Neptune"));
+
+    let (tree,b) = tree.delete("Uranus");
+    println!("{:?} : {}", tree.clone().inorder(), b);
+
+    let (tree,b) = tree.delete("Ganymede");
+    println!("{:?} : {}", tree.clone().inorder(), b);
+
+}
